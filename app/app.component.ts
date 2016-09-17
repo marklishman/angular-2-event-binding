0f08912c9e2f07ef48340cea11b396d023b912b4
Mark <mark.lishman@gmail.com>
2016-09-17 15:51:10 +0100

    Updated to Angular 2.0 Final Release

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
 
-import { MouseComponent } from "./mouse.component";
-import { KeyboardComponent } from "./keyboard.component";
-import { DragDropComponent } from "./drag-drop.component";
-
 @Component({
     selector: 'app',
     template: `
         <mouse *ngIf="showSection('mouse')"></mouse>
         <drag-drop *ngIf="showSection('drag-drop')"></drag-drop>
         <keyboard *ngIf="showSection('keyboard')"></keyboard>
-        `,
-    directives: [
-        MouseComponent,
-        KeyboardComponent,
-        DragDropComponent
-    ]
+        `
 })
 export class AppComponent {
 
---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }   from './app.component';
+import { DragDropComponent } from "./drag-drop.component";
+import { KeyboardComponent } from "./keyboard.component";
+import { MouseComponent } from "./mouse.component";
+
+@NgModule({
+    imports:      [
+        BrowserModule,
+    ],
+    declarations: [
+        AppComponent,
+        DragDropComponent,
+        KeyboardComponent,
+        MouseComponent
+    ],
+    bootstrap:    [
+        AppComponent
+    ]
+})
+export class AppModule { }
